9c3bc556315849b84aac1a311ab3ff0c0c5cdb98
Chubbard022 <chubbard022@gmail.com>
2020-04-09 09:07:27 -0700

    database updating with test records and server is including new recipe routes

--- src/server.js
+++ src/server.js
@@ -12,9 +12,13 @@ const server = express()
 
 server.use(express.json())
 server.use(helmet())
+server.use(cors({
+    //origin: put netlify url here
+    credentials: true
+}))
 
 server.use("/api/inspiration",inspoRoutes)
-// server.use("/api/recipe",recipeMakerRoutes)
+server.use("/api/recipes",recipeMakerRoutes)
 // server.use("/api/social",socialRoutes)
 // server.use("/api/user",userRoutes)
 
