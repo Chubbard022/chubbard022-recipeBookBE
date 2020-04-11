d2d2e9d6cc587f7496db59c551f5e33c2e62e9d8
curtis <chubbard022@gmail.com>
2020-04-11 08:49:33 -0700

    adding routes for finding users and also their recipes

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,18 @@ const knex = require("knex")
 const router = require("express").Router()
 const knexConfig = require("../knexfile")
 const db = knex(knexConfig.development)
+const {authenticate} = require("./auth/authenticate")
 
 
-//get all users
-router.get("/",(req,res)=>{
+router.get("/recipe",authenticate,(req,res)=>{
+    let findUsername = req.decoded.username
     db("users")
-        .then(user=>{
-            res.status(200).json(user)
-        })
-        .catch(err=>console.log("Error: Cannot get users"))
+        .join("recipes","users.username", "=", "recipes.usernames")
+        .where({"recipes.usernames": findUsername})
+    .then(response=>{
+        res.status(200).json(response)
+    })
+    .catch(err=>res.status(400).json(err))
 })
 
 
