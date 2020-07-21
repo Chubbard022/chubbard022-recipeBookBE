b4b06e81067c47d82c6ced06277d06ba9c23b639
curtis <chubbard022@gmail.com>
2020-07-21 08:28:25 -0700

    adding routes and adding table for favorited recipes

--- data/migrations/20200411062250_db.js
+++ data/migrations/20200411062250_db.js
@@ -2,37 +2,35 @@
 exports.up = function(knex) {
     return knex.schema
         .createTable("users", user=>{
-    
             user.increments("id")
-
-            user.string("username",50)
-                .notNullable()
-                .unique()
-            user.string("password",255)
-                .notNullable()
-            
-                
-    })
-    .createTable("inspiration",(tbl)=>{
-        tbl.increments("id")
-        tbl.string("name")
-        tbl.string("ingredients")
-        tbl.string("instructions")
-        tbl.string("image")
-    })
-    .createTable("recipes", recipe=>{
-    
-        recipe.increments("id")
-        recipe.string("name").notNullable()
-        recipe.string("ingredients").notNullable()
-        recipe.string("instructions").notNullable()
-        recipe.string("username")
-                
-    })
+            user.string("username",50).notNullable().unique()
+            user.string("password",255).notNullable()
+        })
+        .createTable("inspiration",(tbl)=>{
+            tbl.increments("id")
+            tbl.string("name")
+            tbl.string("ingredients")
+            tbl.string("instructions")
+            tbl.string("image")
+        })
+        .createTable("recipes", recipe=>{
+            recipe.increments("id")
+            recipe.string("name").notNullable()
+            recipe.string("ingredients").notNullable()
+            recipe.string("instructions").notNullable()
+            recipe.string("username")
+        })
+        .createTable("favorited",favorite=>{
+            favorite.increments("id")
+            favorite.string("name").notNullable()
+            favorite.string("ingredients").notNullable()
+            favorite.string("instructions").notNullable()
+        })
 };
 
 exports.down = function(knex) {
     return knex.schema
+        .dropTableIfExists("favorited")
         .dropTableIfExists("recipes")
         .dropTableIfExists("inspiration")
         .dropTableIfExists("users")
