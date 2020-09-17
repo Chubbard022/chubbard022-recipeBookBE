f766740a746d852bf4d1f061400ffe994cdecbf7
Chubbard022 <chubbard022@gmail.com>
2020-09-17 10:16:06 -0700

    files now in endpoint folder, starting to turn functions into async

---
+++ routes/endpoints/social.js
@@ -0,0 +1,29 @@
+const knex = require("knex")
+const router = require("express").Router()
+const knexConfig = require("../../knexfile")
+const db = knex(knexConfig.development)
+const {authenticate} = require("../auth/authenticate")
+
+
+
+router.get("/",(req,res)=>{
+    db("users")
+        .select("username","id")
+        .then(user=>{
+            res.status(200).json(user)
+        })
+        .catch(err=>console.log("Error: Cannot get users"))
+})
+router.get("/:username",(req,res)=>{
+    let usernameSelected = req.params.username
+    db("users")
+    .join("recipes","users.username", "=", "recipes.username")
+    .then(response=>{
+       let filterResponse =  response.filter(recipe=>{
+            return recipe.username === usernameSelected
+        })
+        res.status(200).json(filterResponse)
+    })
+    .catch(err=>res.status(400).json(err))
+})
+module.exports = router
---
+++ routes/endpoints/user.js
@@ -0,0 +1,22 @@
+const knex = require("knex")
+const router = require("express").Router()
+const knexConfig = require("../../knexfile")
+const db = knex(knexConfig.development)
+const {authenticate} = require("../auth/authenticate")
+
+
+router.get("/recipe",authenticate,(req,res)=>{
+    let findUsername = req.decoded.username
+    db("users")
+        .join("recipes","users.username", "=", "recipes.usernames")
+        .where({"recipes.usernames": findUsername})
+    .then(response=>{
+        res.status(200).json(response)
+    })
+    .catch(err=>res.status(400).json(err))
+})
+
+
+
+
+module.exports = router
