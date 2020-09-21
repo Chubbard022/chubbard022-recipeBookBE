47ca10f848e6fc4803208e107014c1597d6da0bf
Chubbard022 <chubbard022@gmail.com>
2020-09-21 08:52:50 -0700

    deleting helper function not completed or be used

--- routes/helperFunctions/pantryHelper.js
+++ routes/helperFunctions/pantryHelper.js
@@ -1,11 +1,10 @@
-const knex = require("knex");
-const knexConfig = require("../../knexfile");
-const db = knex(knexConfig.development);
+const knex = require("knex")
+const knexConfig = require("../../knexfile")
+const db = knex(knexConfig.development)
 
-module.exprorts = {
+module.exports = {
     find,
     findByName,
-    recipeUsingPantry,
     add,
     update,
     remove
@@ -19,30 +18,26 @@ async function find(){
 //returns a pantry item given passed in item name
 async function findByName(itemName){
     return await db("pantryItems")
-                .where({nameOfItem:itemName});
-}
-
-// returns recipe....
-async function recipeUsingPantry(){
-    //unknown yet 
+                .where({nameOfItem:itemName})
+                .first()
 }
 
 //adds item to pantry list
 async function add(newItem){
-    const [itemName] = await db("nameOfItem")
-                        .insert(newItem);
-    return await findByName(itemName);
+    await db("pantryItems")
+            .insert(newItem);
+    return await findByName(newItem.nameOfItem);
 }
 
 //update pantry item given passed id and updated pantry item
 async function update(id,updatedPantryItem){
-    return await db("nameOfItem")
+    return await db("pantryItems")
             .where({id})
             .update(updatedPantryItem)
 }
 // removes item from list of pantry items
 async function remove(id){
-    return await db("nameOfItem")
+    return await db("pantryItems")
             .where({id})
             .del()
 }
