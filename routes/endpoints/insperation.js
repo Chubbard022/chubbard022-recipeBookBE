886d0530ee8adaa9da336e2c32fd5d67957b6d76
Chubbard022 <chubbard022@gmail.com>
2020-09-28 06:38:21 -0700

    adding endpoints for insperation routes

--- routes/endpoints/insperation.js
+++ routes/endpoints/insperation.js
@@ -1,32 +1,79 @@
 require("dotenv").config()
-const axios = require("axios")
-const knex = require("knex")
-const router = require("express").Router()
-const knexConfig = require("../../knexfile")
-const db = knex(knexConfig.development)
-const {authenticate} = require("../auth/authenticate")
+const router = require("express").Router();
+const inspirationHelper = require("../helperFunctions/insperationHelper");
 
-router.post(`/newrecipe`, (req,res,next)=>{
-    let nextRecipe = req.body
-    db("inspiration").insert(nextRecipe)
-        .then(response=>{
-            res.status(201).json({responseMessage: "Successfully found new recipe"})
+router.post(`/newrecipe`, async (req,res)=>{
+    try{    
+        let newRecipe = req.body;
+        let addRecipeToInspiration = await inspirationHelper.add(newRecipe);
+
+        res.status(200).json(addRecipeToInspiration);
+    }catch(error){
+        res.status(500).json({
+            errorMessage:
+            "Sorry something went wrong adding recipe to insperation"
         })
-        .catch(err=>res.status(400).json(err))
+        throw new Error(error)
+    }
 })
 
-router.get("/",(req,res)=>{
-    db("inspiration")
-        .then(data=>res.status(200).json(data))
-        .catch(err=>res.status(400).json(err))
+router.get("/", async (req,res)=>{
+    try{
+        let inspirationList = inspirationHelper.find();
+        res.status(200).json(inspirationList)
+    }
+    catch(error){
+        res.status(500).json({
+            errorMessage: "Sorry, something went wrong getting insperation recipes"
+        })
+        throw new Error(error);
+    }
 })
 
-router.put("/:id",(req,res)=>{
-    db("inspiration")
-        .where({id:req.params.id})
-        .update(req.body)
-            .then(response=>res.status(200).json(response))
-            .catch(err=>res.status(400).json(err))
+router.get("/:id", async (req,res)=>{
+    try{
+        let insprirationId = req.params.id;
+        let getInspirationById = inspirationHelper.findById(insprirationId);
+
+        res.status(200).json(getInspirationById);
+    }
+    catch(error){
+        res.status(500).json({
+            errorMessage : 
+            "Sorry something went wrong getting recipe"
+        })
+        throw new Error(error);
+    }
 })
 
+router.put("/:id",async (req,res)=>{
+    try{
+        let recipeId = req.params.id;
+        let updatedInspo = req.body;
+        let updateInspo = inspirationHelper.update(recipeId,updatedInspo)
+
+        res.status(200).json(updateInspo);
+    }catch(error){
+        res.status(500).json({
+            errorMessage:
+            "Sorry something went wrong with udating recipe"
+        })
+        throw new Error(error);
+    }
+})
+
+router.delete("/:id", async (req,res)=>{
+    try{
+        let recipeId = req.params.id;
+        let deleteInspo = inspirationHelper.remove(recipeId);
+
+        res.status(200).json(deleteInspo);
+    }catch(error){
+        res.status(500).json({
+            errorMessage:
+            "Sorry something went wrong deleting recipe"
+        })
+        throw new Error(error);
+    }
+})
 module.exports = router;
